3af2c9e0d19a47dfa2963aa91f205f1f7eb9a5ad
Kaer Tala <khair_talap@outlook.com>
2025-05-28 08:49:13 +0500

    Addition of counters and metrics

--- src/handlers.go
+++ src/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math/rand/v2"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -16,16 +18,27 @@ var (
 		Name: "hello_world_requests",
 		Help: "Total number of Hello World requests received",
 	})
+
+	exceptions = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "hello_world_exceptions_total",
+		Help: "Exceptions serving Hello World.",
+	})
 )
 
 func init() {
 	// Register metrics with prometheus
 	prometheus.MustRegister(helloWorldsRequested)
 	prometheus.MustRegister(helloWorldsTotal)
+	prometheus.MustRegister(exceptions)
 }
 
 func helloHandler(c *fiber.Ctx) error {
 	helloWorldsRequested.Inc()
+
+	if rand.Float64() < 0.2 {
+		exceptions.Inc()
+		return fiber.NewError(fiber.StatusInternalServerError, "Random exception occured")
+	}
 
 	defer helloWorldsTotal.Inc()
 
